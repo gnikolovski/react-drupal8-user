87572c22f6c26d11bc81c0777fb0d745f6ef3ebb
Goran Nikolovski <nikolovski84@gmail.com>
2017-05-02 19:38:51 +0200

    Render User Profile component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import UserRegister from './components/UserRegister.jsx';
 import UserLogin from './components/UserLogin.jsx';
+import UserProfile from './components/UserProfile.jsx';
 
 class App extends Component {
   render() {
@@ -16,6 +17,12 @@ class App extends Component {
         <div className="row">
           <div className="col-md-6 offset-md-3">
             <UserLogin baseUrl="http://www.your-drupal-8-backend.com" />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <UserProfile baseUrl="http://www.your-drupal-8-backend.com" />
           </div>
         </div>
       </div>
